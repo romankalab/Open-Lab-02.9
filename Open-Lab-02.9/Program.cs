23c0e3e5f1c33e251b3416f861cb924b318e09a4
romankalab <roan.kalab@mail.com>
2019-10-16 00:03:56 +0200

    Initial

---
+++ Open-Lab-02.9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Open_Lab_02._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your first name:");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Type your last name:");
+            string last = Console.ReadLine();
+
+            string fullname = ConcatName(first, last);
+
+            Console.WriteLine($"This is your full name: {fullname}");
+        }
+        private static string ConcatName(string first, string last)
+        {
+            return last + ", " + first;
+        }
+    }
+}
